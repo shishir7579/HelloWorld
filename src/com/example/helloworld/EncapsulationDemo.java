ec43f2807c65fb5489d281501905e633128aeab9
shishir <shishir@localhost.localdomain>
2021-05-09 20:15:25 +0545

    line break

--- src/com/example/helloworld/EncapsulationDemo.java
+++ src/com/example/helloworld/EncapsulationDemo.java
@@ -4,7 +4,6 @@ public class EncapsulationDemo extends abc{
     public static void main(String[] args) {
         EncapsulationDemo obj1 = new EncapsulationDemo();
         obj1.test();
-
     }
 }
 
